370f59d4be6be2e53adb60cfc077b29f3b419aad
ashishoia <131574266+ashishoia@users.noreply.github.com>
2025-11-03 17:45:09 +0530

    Refactor movie recommendation logic and API calls

--- app.py
+++ app.py
@@ -1,60 +1,114 @@
-import pickle
 import streamlit as st
+import pandas as pd
+import numpy as np
+import pickle
 import requests
+import time
 
-def fetch_poster(movie_id):
-    url = "https://api.themoviedb.org/3/movie/{}?api_key=8265bd1679663a7ea12ac168da84d2e8&language=en-US".format(movie_id)
-    data = requests.get(url)
-    data = data.json()
-    poster_path = data['poster_path']
-    full_path = "https://image.tmdb.org/t/p/w500/" + poster_path
-    return full_path
-
-def recommend(movie):
-    index = movies[movies['title'] == movie].index[0]
-    distances = sorted(list(enumerate(similarity[index])), reverse=True, key=lambda x: x[1])
-    recommended_movie_names = []
-    recommended_movie_posters = []
-    for i in distances[1:6]:
-        # fetch the movie poster
-        movie_id = movies.iloc[i[0]].movie_id
-        recommended_movie_posters.append(fetch_poster(movie_id))
-        recommended_movie_names.append(movies.iloc[i[0]].title)
-
-    return recommended_movie_names,recommended_movie_posters
+movies = pickle.load(open("model/movie_list.pkl", "rb"))
+similar = pickle.load(open("model/similarity.pkl", "rb"))
+movies_df = pd.DataFrame(movies)
 
 
-st.header('Movie Recommender System')
-movies = pickle.load(open('model/movie_list.pkl','rb'))
-similarity = pickle.load(open('model/similarity.pkl','rb'))
+def fetch_poster(movies_id):
+    tmdb_id = movies_id
+    api_key = "330018326c4140e05777de814498a62a"
 
-movie_list = movies['title'].values
-selected_movie = st.selectbox(
-    "Type or select a movie from the dropdown",
-    movie_list
+    url = f"https://api.themoviedb.org/3/movie/{tmdb_id}?api_key={api_key}"
+
+    headers = {
+        "accept": "application/json",
+        "Authorization": "Bearer 330018326c4140e05777de814498a62a"
+    }
+
+    response = requests.get(url, headers=headers)
+    data = response.json()
+
+    return "https://image.tmdb.org/t/p/w500/" + data['poster_path']
+
+
+def fetch_link(movies_id):
+    tmdb_id = movies_id
+    api_key = "330018326c4140e05777de814498a62a"
+
+    url = f"https://api.themoviedb.org/3/movie/{tmdb_id}?api_key={api_key}"
+
+    headers = {
+        "accept": "application/json",
+        "Authorization": "Bearer 330018326c4140e05777de814498a62a"
+    }
+
+    response = requests.get(url, headers=headers)
+    data = response.json()
+
+    return data['homepage']
+
+
+def recommendation(movie_name):
+    movie_index = movies_df[movies_df['title'] == movie_name].index[0]
+    distances = similar[movie_index]
+    movies_list = sorted(list(enumerate(distances)), reverse=True, key=lambda x: x[1])[
+                  1:6]  # it returns an array of tuples
+
+    poster = []
+    for j in movies_list:
+        movies_id = movies_df.iloc[j[0]].movie_id
+        poster.append(fetch_poster(movies_id))
+
+    link = []
+    for k in movies_list:
+        movies_id = movies_df.iloc[k[0]].movie_id
+        link.append(fetch_link(movies_id))
+    movies_recommended = []
+    for i in movies_list:  # i is a tuple here
+        movies_recommended.append(movies_df.iloc[i[0]].title)  # i[0] will give me the index of that movie
+    return movies_recommended, poster, link
+
+
+disclaimer_message = (
+    "Dear User,\n\n"
+    "We are committed to providing you with the best movie recommendations based on the available data. "
+    "We are constantly working to enhance our movie database and recommend the latest films. "
+    "However, please note that the dataset may not be exhaustive, and there might be chances of errors or omissions. "
+    "We appreciate your understanding and apologize for any inconvenience caused."
 )
 
-if st.button('Show Recommendation'):
-    recommended_movie_names,recommended_movie_posters = recommend(selected_movie)
-    col1, col2, col3, col4, col5 = st.columns(5)
+
+
+
+st.title(':grey[_Movie_]  :red[Recommender] :sunglasses:')
+
+selected_movie = st.sidebar.selectbox('Select movies to recommend', movies_df['title'].values)
+
+if st.button('Recommend movie'):
+    name, poster1, link1 = recommendation(selected_movie)
+    col1, col2, = st.columns(2)
     with col1:
-        st.text(recommended_movie_names[0])
-        st.image(recommended_movie_posters[0])
+        st.markdown(f"<div style='white-space: pre-wrap;'>{name[0]}</div>", unsafe_allow_html=True)
+        st.markdown(f"<img src='{poster1[0]}' style='border-radius: 10px; max-width: 100%;' />", unsafe_allow_html=True)
+        st.markdown(link1[0], unsafe_allow_html=True)
     with col2:
-        st.text(recommended_movie_names[1])
-        st.image(recommended_movie_posters[1])
-
+        st.markdown(f"<div style='white-space: pre-wrap;'>{name[1]}</div>", unsafe_allow_html=True)
+        st.markdown(f"<img src='{poster1[1]}' style='border-radius: 10px; max-width: 100%;' />", unsafe_allow_html=True)
+        st.markdown(link1[1], unsafe_allow_html=True)
+    col3, col4 = st.columns(2)
     with col3:
-        st.text(recommended_movie_names[2])
-        st.image(recommended_movie_posters[2])
+        st.markdown(f"<div style='white-space: pre-wrap;'>{name[2]}</div>", unsafe_allow_html=True)
+        st.markdown(f"<img src='{poster1[2]}' style='border-radius: 10px; max-width: 100%;' />", unsafe_allow_html=True)
+        st.markdown(link1[2], unsafe_allow_html=True)
     with col4:
-        st.text(recommended_movie_names[3])
-        st.image(recommended_movie_posters[3])
+        st.markdown(f"<div style='white-space: pre-wrap;'>{name[3]}</div>", unsafe_allow_html=True)
+        st.markdown(f"<img src='{poster1[3]}' style='border-radius: 10px; max-width: 100%;' />", unsafe_allow_html=True)
+        st.markdown(link1[3], unsafe_allow_html=True)
+    col5, col6 = st.columns(2)
     with col5:
-        st.text(recommended_movie_names[4])
-        st.image(recommended_movie_posters[4])
+        st.markdown(f"<div style='white-space: pre-wrap;'>{name[4]}</div>", unsafe_allow_html=True)
+        st.markdown(f"<img src='{poster1[4]}' style='border-radius: 10px; max-width: 100%;' />", unsafe_allow_html=True)
+        st.markdown(link1[4], unsafe_allow_html=True)
 
 
-
-
-
+if st.button("Disclaimer"):
+    disclaimer_placeholder = st.empty()
+    disclaimer_placeholder.info(disclaimer_message)
+    time.sleep(5)  # Adjust the delay as needed
+    disclaimer_placeholder.empty()
